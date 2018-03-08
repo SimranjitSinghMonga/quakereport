d28226000281d933c23c38bc396cc3355a6d5a99
Sim <simran_monga2003@hotmail.com>
2018-03-08 23:30:05 +0530

    JSonParsing in String Format

---
+++ app/src/main/java/com/example/sim/quakereport/Earthquake.java
@@ -0,0 +1,29 @@
+package com.example.sim.quakereport;
+
+/**
+ * Created by Sim on 08/03/2018.
+ */
+public class Earthquake {
+    private String mMagnitude;
+    private String mLocation;
+    private String mDate;
+
+    public Earthquake(String magnitude,String location,String date)
+    {
+      mMagnitude=magnitude;
+        mLocation=location;
+        mDate=date;
+    }
+    public String getMagnitude()
+    {
+        return mMagnitude;
+    }
+    public String getLocation()
+    {
+        return mLocation;
+    }
+    public String getDate()
+    {
+        return mDate;
+    }
+}
--- app/src/main/java/com/example/sim/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/sim/quakereport/EarthquakeActivity.java
@@ -7,32 +7,25 @@ import android.widget.ListView;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class EarthquakeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.earthquake_activity);
         // Create a fake list of earthquake locations.
-        ArrayList<String> earthquakes = new ArrayList<>();
-        earthquakes.add("San Francisco");
-        earthquakes.add("London");
-        earthquakes.add("Tokyo");
-        earthquakes.add("Mexico City");
-        earthquakes.add("Moscow");
-        earthquakes.add("Rio de Janeiro");
-        earthquakes.add("Paris");
+        ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes();
 
         // Find a reference to the {@link ListView} in the layout
         ListView earthquakeListView = (ListView) findViewById(R.id.list);
 
-        // Create a new {@link ArrayAdapter} of earthquakes
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-                this, android.R.layout.simple_list_item_1, earthquakes);
+        // Create a new adapter that takes the list of earthquakes as input
+        final EarthquakeAdapter adapter = new EarthquakeAdapter(this, earthquakes);
 
         // Set the adapter on the {@link ListView}
         // so the list can be populated in the user interface
         earthquakeListView.setAdapter(adapter);
+
 
     }
 }
